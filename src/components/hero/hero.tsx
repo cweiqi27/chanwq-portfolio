c9a89ae7e13b4444d5b6e02207bebbc7492a70fe
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-23 05:09:40 +0800

    Add hero background, hero ui changes

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,22 +1,30 @@
 import styles from './hero.module.scss';
 import { ParallaxLayer } from "@react-spring/parallax";
+import { useSpring, animated, config } from "@react-spring/web";
 
 const Hero = () => {
+  const float = useSpring({
+    from: { scaleX: 0 },
+    scaleX: 1,
+    skewX: 4,
+    translateY: 20,
+    config: config.molasses,
+  });
   return (
     <ParallaxLayer
       offset={0}
       speed={-0.5}
-      className={styles.hero}
+      className={styles.heroContainer}
     >
+      <section className={styles.hero}>
+        <div className={styles.glass}></div>
         <h1>
           Hello world, I&apos;m <span className={styles.name}>Wei Qi</span>.
         <br/>
           I develop&nbsp;
-          <span className={styles.block}>
-            <span className={styles.highlight}></span>
-            <span className={styles.keyword}>Web Apps</span>
-          </span>
+        <span className={styles.keyword}>Web Apps</span>
         </h1>
+      </section>
     </ParallaxLayer>
   );
 }
---
+++ src/components/hero/heroBackground/heroBackground.tsx
@@ -0,0 +1,12 @@
+import { useSpring, animated } from "@react-spring/web";
+import styles from "./heroBackground.module.scss"
+
+const HeroBackground = () => {
+  return (
+    <div
+      className={styles.heroBackground}
+    />
+  );
+}
+
+export default HeroBackground;
