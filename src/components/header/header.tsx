d135fe19c658bb11eb6213495b58ec1a5a34e26e
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-22 14:52:38 +0800

    Refactor header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,15 @@
 import styles from './header.module.scss';
+import {ParallaxLayer} from "@react-spring/parallax";
 
 const Header = () => {
   return (
-    <header>
-      <nav className={styles.header}>
-        <div>
-          <h1>chan</h1>
-        </div>
-        <h2>something</h2>
-        <h2>something</h2>
-      </nav>
-    </header>
+    <ParallaxLayer factor={0.1} offset={0} speed={2} className={styles.header}>
+          <div>
+            <h1>chanwq</h1>
+          </div>
+          <h2>something</h2>
+          <h2>something</h2>
+    </ParallaxLayer>
   );
 }
 
