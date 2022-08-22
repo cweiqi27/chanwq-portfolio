c1befb6df3c0c906a62cbb4693a033dc078c3458
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-23 05:08:43 +0800

    Add skills section background

---
+++ src/components/skills/skillsBackground/skillsBackground.tsx
@@ -0,0 +1,10 @@
+import styles from "./skillsBackground.module.scss";
+import { useSpring, animated } from "@react-spring/web";
+
+const SkillsBackground = () => {
+  return (
+    <div className={styles.skillsBackground} />
+  );
+}
+
+export default SkillsBackground;
