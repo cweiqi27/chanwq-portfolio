7a3c8b62f97b38717392f808ab847e19d5c61bd9
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-22 14:51:30 +0800

    Add circle

---
+++ src/components/circle/circle.tsx
@@ -0,0 +1,24 @@
+import styles from "./circle.module.scss";
+import { ParallaxLayer } from "@react-spring/parallax";
+import { useSpring, animated, config } from "@react-spring/web";
+
+const Circle = () => {
+  const float = useSpring({
+    from: { x: -60, y: -35 },
+    to: { x: -20, y: -10 },
+    config: config.molasses,
+    loop: { reverse: true }
+  });
+  return (
+    <ParallaxLayer
+      offset={0}
+      speed={-0.1}
+      factor={1}
+      className={styles.circleContainer}
+    >
+      <animated.div className={styles.circle} style={float}></animated.div>
+    </ParallaxLayer>
+  );
+}
+
+export default Circle;
