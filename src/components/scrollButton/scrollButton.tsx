046ab3e54d1e81198b911e5297a67fa351481461
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-22 14:47:15 +0800

    Add scroll button

---
+++ src/components/scrollButton/scrollButton.tsx
@@ -0,0 +1,50 @@
+import styles from "./scrollButton.module.scss";
+import { ParallaxLayer } from "@react-spring/parallax";
+import { useSpring, animated, config } from "@react-spring/web";
+import { useState } from "react";
+
+type ScrollButtonProps = {
+  offset: number;
+  speed: number;
+  factor: number;
+  position: string;
+  onClick: () => void;
+}
+
+const ScrollButton = ({ offset, speed, factor, onClick }: ScrollButtonProps) => {
+  const [flip, set] = useState(false);
+  const scroll = useSpring({
+    from: { top: '0.5rem' },
+    to: { top: '3.5rem' },
+    config: () => flip ? config.molasses : config.slow,
+    loop: { reverse: true },
+    onRest: () => set(!flip)
+  });
+  const blip = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 0.6 },
+    delay: 2000,
+    config: config.molasses
+  });
+  return (
+    <ParallaxLayer
+      offset={offset}
+      speed={speed}
+      factor={factor}
+      onClick={onClick}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        margin: '0 auto',
+        maxWidth: '1000px'
+      }}
+    >
+      <animated.div className={styles.ring} style={blip}>
+        <animated.div className={styles.ball} style={scroll}/>
+      </animated.div>
+    </ParallaxLayer>
+  );
+}
+
+export default ScrollButton;
