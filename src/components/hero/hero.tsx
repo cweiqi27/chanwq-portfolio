b12f798285db316c5e11b7adee3f2bef12afef65
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-22 14:50:03 +0800

    Refactor hero section

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,19 +1,23 @@
 import styles from './hero.module.scss';
+import { ParallaxLayer } from "@react-spring/parallax";
 
 const Hero = () => {
   return (
-    <section className={styles.hero}>
-      <h1>
-        Hello world, I&apos;m <span className={styles.keyword}>Wei Qi</span>.
-      </h1>
-      <h1>
-        I develop&nbsp;
-        <span className={styles.block}>
-          <span className={styles.highlight}></span>
-          <span className={styles.keyword}>Web Apps</span>
-        </span>
-      </h1>
-    </section>
+    <ParallaxLayer
+      offset={0}
+      speed={-0.5}
+      className={styles.hero}
+    >
+        <h1>
+          Hello world, I&apos;m <span className={styles.name}>Wei Qi</span>.
+        <br/>
+          I develop&nbsp;
+          <span className={styles.block}>
+            <span className={styles.highlight}></span>
+            <span className={styles.keyword}>Web Apps</span>
+          </span>
+        </h1>
+    </ParallaxLayer>
   );
 }
 
